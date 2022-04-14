bf245e3cab91d9ecc915d457a4c4dfbf11d6a8f5
Baugrems <scopenhagen@mines.edu>
2022-04-14 15:13:53 -0600

    comments for week 12

--- week12/coursescheduling.py
+++ week12/coursescheduling.py
@@ -1,23 +1,23 @@
 def countCourses(inputList):
     courses = {}
-    studCourses = {}   
-    for course in inputList:
-        course = course.split()
+    studCourses = {}   #empty maps for setup. One for courses and one for students
+    for course in inputList: 
+        course = course.split() # split the input and separate student from course
         student = course[0] + " " + course[1]
         course = course[-1]
-        if student not in studCourses:
+        if student not in studCourses: # if student is not in map, add them
             studCourses[student] = []
-        if course in studCourses[student]:
+        if course in studCourses[student]: # if they already added this course, skip and continue loop
             continue
         else:
-            studCourses[student].append(course)
-        if course not in courses:
+            studCourses[student].append(course) # put the course in the students list of courses
+        if course not in courses: # now we add course to our courses map if it isnt there yet
             courses[course] = 1
         else:
-            courses[course] += 1
+            courses[course] += 1 # or we increment the count in the map for that course
         courseKeys = list(courses.keys())
-        courseKeys.sort()
-    for key in courseKeys:
+        courseKeys.sort() # sort all the courses for correct ordering
+    for key in courseKeys: # print them all with their accumulated values
         print(key, courses[key])
 
 
--- week12/unbearablezoo.py
+++ week12/unbearablezoo.py
@@ -2,28 +2,29 @@
 def findAnimalCounts(animalList):
     global animals
     for animal in animalList:
-        if animal in animals:
-            animals[animal] += 1
+        if animal in animals: # checking if key exists in animals map
+            animals[animal] += 1 # increment count by 1 if so
         else:
-            animals[animal] = 1
+            animals[animal] = 1 # else start it at 1
     keys = list(animals.keys())
-    keys.sort()
-    for key in keys:
+    keys.sort() # sort for alphabetical order so we list it correctly
+    for key in keys: # print out in order the key and the value
         print(key, "|", animals[key])
+
 animals = {}
 if __name__ == '__main__':
 
     amt = 999
     listNum = 0
-    while amt != 0:
-        listNum += 1
+    while amt != 0: # keep accepting input until we dont have any animals wanted
+        listNum += 1 # keep track of how many lists we've done
         amt = int(input())
         animals = {}
         animalList = []
         for n in range(amt):
-            animal = input().split()
-            animal = animal[-1].lower()
-            animalList.append(animal)
+            animal = input().split() # split by word
+            animal = animal[-1].lower() # we only care about last word, the animal type
+            animalList.append(animal) # add to our list, move on
         if amt:
-            print("List " + str(listNum) + ":")
-        findAnimalCounts(animalList)
+            print("List " + str(listNum) + ":") # we need to for some reason output a list indictator here. So we do
+        findAnimalCounts(animalList) 
